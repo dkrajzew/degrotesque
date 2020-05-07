c401262a277dd46e5d342b0286ec42110b31632c
dkrajzew <d.krajzewicz@gmail.com>
2020-05-07 22:15:20 +0200

    building a package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="degrotesque",
+    version="0.3",
+    author="dkrajzew",
+    author_email="d.krajzewicz@gmail.com",
+    description="A tiny web type setter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dkrajzew/degrotesque",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
