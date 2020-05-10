4c88aa42b83ebf4accc43869974ebae7ea778f74
dkrajzew <d.krajzewicz@gmail.com>
2020-05-10 13:14:08 +0200

    refactoring

--- degrotesque/degrotesque.py
+++ degrotesque/degrotesque.py
@@ -133,74 +133,15 @@ extensionsDB = [
   "ppthtml",   
   "ssp", "jhtml" ]
 
+"""List of elements which coontents shall not be processed"""
+elementsToSkip = [
+ u"script", u"code", u"style", u"pre", u"?", u"?php"
+]
+
+
 
 # --- methods -------------------------------------------------------
 # -- degrotesquing
-def action_use(action, html, i):
-  """Applies an action to the given HTML snipplet if the action matches.
-  
-  Returns the next index or -1 if the action could not be applied."""
-  if html[i:i+len(action[0][0])]!=action[0][0]:
-    return -1, html # does not match
-  if len(action[1][0])==0:
-    html = html[:i] + action[1][1] + html[i+len(action[0][0]):]
-    return i - len(action[0][0]) + len(action[1][1]), html
-  j = i + len(action[0][0])
-  opened = False
-  while j<len(html):
-    if opened:
-      if html[j]=='>':
-        opened = False
-      j = j + 1
-      continue
-    if html[j]=='<':
-      opened = True
-      j = j + 1
-      continue
-    if html[j:j+len(action[1][0])]!=action[1][0]:
-      j = j + 1
-      continue
-    html = html[:j] + action[1][2] + html[j+len(action[1][0]):]
-    html = html[:i] + action[1][1] + html[i+len(action[0][0]):]
-    return i - len(action[0][0]) + len(action[1][1]), html
-  return -1, html
-
-
-
-def skipIfCode(html, i):
-  """Skips a part of the HTML snipplet if it shall be not processed.
-  
-  This counts for script, style, and pre elements."""
-  toFind = None
-  i = i + 1
-  if html[i:i+6].lower()=="script":
-    toFind = "</script"       
-    i = i + 6
-  if html[i:i+4].lower()=="code":
-    toFind = "</code"
-    i = i + 4
-  if html[i:i+5].lower()=="style":
-    toFind = "</style"
-    i = i + 5
-  if html[i:i+3].lower()=="pre":
-    toFind = "</pre"
-    i = i + 3
-  if html[i:i+1]=="?":
-    toFind = "?>"
-    i = i + 1
-  if toFind==None:
-    return i, True
-  while i<len(html) and html[i:i+len(toFind)]!=toFind:
-    i = i + 1
-  if html[i:i+len(toFind)].lower()!=toFind:
-    raise ValueError("Could not find matching end of '%s'" % toFind)
-  if toFind!="?>":
-    while i<len(html) and html[i]!='>':
-      i = i + 1
-  return i + 1, False
-   
-
-
 def getTagName(html):
   i = 0
   while i<len(html) and ord(html[i])<=32:
@@ -214,15 +155,10 @@ def getTagName(html):
   return html[ib:ie]      
    
 
-elementsToSkip = [
- u"script", u"code", u"style", u"pre", u"?", u"?php"
-]
-
 
 def mark(html):
   opened = 0
-  #print (len(html))
-  # mark first
+  # mark HTML elements, first
   ret = ""
   for i in range(0, len(html)):
     if opened==0:
@@ -237,19 +173,12 @@ def mark(html):
   # mark code parts
   i = 0
   while i<len(html):
-    #print ("%s %s" % (i, len(html)))
     if html[i]=='<':
-      #print (html[i-50:i+50])
-      #if i==145:
-      #print (html[i+1:i+50]) 
       tb = getTagName(html[i+1:])
-      #print (len(tb))
-      #print ("'" + tb + "' " + str(i))
       if tb not in elementsToSkip:
         i = i + len(tb)
         continue
       if tb=="?" or tb=="?php":
-        #print("here")
         ib = i
         ie = html.index("?>", ib)
       else:
@@ -258,7 +187,6 @@ def mark(html):
         ib = i
         while i<len(html) and ib<=i:
           n = html.find("/"+tb, i)
-          #print (tb + "  " + str(n) + "  " + str(i))
           if n<0:
             print ("unclosed element")
             print (tb)
@@ -266,101 +194,32 @@ def mark(html):
           if n<ib:
             print ("false")
             break
-          #print (html[n-5:n+50])
           te = getTagName(html[n:])
-          #print ("te: %s" % te)
           if tb==te:
             ie = html.index(">", n+len(te))
-            #print ("ie: %s" % ie)
             break
           print ("Nope")
-      #print ("a1 %s %s" % (ib, ie))
       assert(len(ret)==len(html))
       if ib>=0 and ie>=0:
         ret = ret[:ib] + "1"*(ie-ib) + ret[ie:]
       assert(len(ret)==len(html))
       i = ie - 1
-      #print ("a2 %s %s %s" % (ib, ie, i))
-      #print ("a2 %s" % (i))
     i = i + 1
     assert(len(ret)==len(html))
   return ret      
-        
-        
-    
-  
-  
 
-  
-          
-    
 
-   
-
-"""
-def getTextAndIndices(html):
-  retStr = ""
-  retIdx = []
-  opening = 0
-  codeBlock = []
-  i = 0
-  while i<len(html):
-    j = html.find("<", i)
-    if j<0: j = len(html)
-    print (j)
-    if j!=i:
-      retStr += html[i:j]
-      retIdx.extend(range(i, j))
-    opening = 1
-    cb = None
-    if html[i+1:].startswith("script"):
-      cb = "script" 
-    elif html[i+1:].startswith("code"):
-      cb = "code" 
-    elif html[i+1:].startswith("style"):
-      cb = "style" 
-    elif html[i+1:].startswith("pre"):
-      cb = "pre" 
-    elif html[i+1]=='?':
-      cb = "?" 
-    #print ("cb: %s" % cb)
-    if cb!=None:
-      if cb=='?':
-        j = html.find("?>", j+len(cb))
-        j = j + 2
-      else:
-        j = html.find("/"+cb+">", j+len(cb))
-        j = j + len(cb) + 2
-    else:
-      j = j + 1
-      #print ("Here %s %s %s" % (i, j, len(html)))
-      while j<len(html):
-        #print ("%s %s %s" % (j, html[j], opening))
-        if html[j]=='<':
-          opening = opening + 1
-        elif html[j]=='>':
-          opening = opening - 1
-        j = j + 1
-        if opening==0:
-          break
-      #print ("Here %s %s %s" % (i, j, len(html)))
-      i = j
-  print (retStr)
-  return retStr, retIdx
-"""
-
-def prettify(html, marks, actions, log=[]): # empty, dismissed log if nothing's passed as reference
+def prettify(html, actions):
   """Prettifies (degrotesques) the given HTML snipplet using the given actions.
   
   It is assumed that the input is given in utf-8.
   The result is returned in utf-8 as well.
   """
-  #print ("a1")
-  #print (marks)
   i = 0
+  # extract text parts
+  marks = mark(html)
   assert(len(html)==len(marks))
   while i<len(html):
-    #print ("%s %s" % (i, len(html)))
     if marks[i]=="1":
       i = i + 1
       continue
@@ -372,7 +231,7 @@ def prettify(html, marks, actions, log=[]): # empty, dismissed log if nothing's 
       closing = None
       if a[0][1]!=None:
         while True:
-          closing = re.match(a[0][1], html[ib:])
+          closing = re.search(a[0][1], html[ib:])
           if not closing:
             break
           if marks[ib+closing.start():ib+closing.end()].find("1")<0:
@@ -381,55 +240,17 @@ def prettify(html, marks, actions, log=[]): # empty, dismissed log if nothing's 
           closing = None
       if a[0][1]!=None and closing==None:
         continue
-      #print (a)
-      #print (opening.group(0))
-      #print (opening.span(0))
       if closing!=None:
         html = html[:ib] + re.sub(a[0][1], a[1][1], html[ib:], 1)
-        marks = marks[:ib+closing.begin()] + "0"*len(a[1][1]) + marks[ib+closing.end():]
+        marks = marks[:ib+closing.start()] + "0"*len(a[1][1]) + marks[ib+closing.end():]
         assert(len(html)==len(marks))
       html = html[:i] + re.sub(a[0][0], a[1][0], html[i:], 1)
-      #print ("0"*len(a[1][0]))
       marks = marks[:i] + "0"*len(a[1][0]) + marks[i+opening.end():]
-      #print (len(html))
-      #print (len(marks))
       assert(len(html)==len(marks))
       break
     i = i + 1
   return html
   
-  
-"""  
-  i = 0
-  opened = False
-  oN = 0
-  findEnd = None
-  while i<len(html): # iterate over the document
-    if opened:
-      if html[i]=='>':
-        oN -= 1
-      elif html[i]=='<':
-        oN += 1
-      if oN==0:
-        opened = False
-      i = i + 1
-      continue
-    if html[i]=='<':
-      i, opened = skipIfCode(html, i)
-      if opened:
-        oN += 1
-      continue
-    found = False
-    for a in actions:
-      nextIndex, html = action_use(a, html, i)
-      if nextIndex<0:
-        continue
-      found = True
-      i = nextIndex
-    if not found:
-      i = i + 1
-  return html      
-"""
 
 
 # -- options parsing
@@ -438,7 +259,6 @@ def getExtensions(extNames):
   if extNames==None or len(extNames)==0:
     return extensionsDB
   return extNames.split(",")
-
 
 
 def getActions(actNames):
@@ -457,11 +277,7 @@ def getActions(actNames):
       actions.extend(actionsDB[an])
     else:
       raise ValueError("Action '%s' is not known." % (an))
-#  for ia,a in enumerate(actions):
-#    if actions[ia][0][0]!=None: 
-#      actions[ia][0][0] = re.compile(actions[ia][0][0])
   return actions
-
 
 
 def getFiles(name, recursive, extensions):
@@ -513,18 +329,8 @@ def main(call):
       html = fd.read()
       fd.close()
       html = html.encode("utf-8", "ignore")
-      
-      # extract text parts
-      marks = mark(html)
-      assert(len(html)==len(marks))
-      #fdo = open("t2.txt", "w")
-      #fdo.write(marks)
-      #fdo.close()
-      
       # apply the beautifications
-      html = prettify(html, marks, actions)
-      
-      
+      html = prettify(html, actions)
       # save it
       html = html.decode(options.encoding)
       if not options.no_backup:
