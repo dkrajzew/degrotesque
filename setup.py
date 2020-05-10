c2061556c8b6806a408be1e2eff006e6dc96dd68
dkrajzew <d.krajzewicz@gmail.com>
2020-05-10 19:08:22 +0200

    adding tests and some further functionality

--- setup.py
+++ setup.py
@@ -18,25 +18,25 @@ import setuptools
 
 # --- definitions ---------------------------------------------------
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+  long_description = fh.read()
 
 setuptools.setup(
-    name="degrotesque",
-    version="0.4",
-    author="dkrajzew",
-    author_email="d.krajzewicz@gmail.com",
-    description="A tiny web type setter",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/dkrajzew/degrotesque",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=2.7, <4',
+  name="degrotesque",
+  version="0.6",
+  author="dkrajzew",
+  author_email="d.krajzewicz@gmail.com",
+  description="A tiny web type setter",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/dkrajzew/degrotesque",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=2.7, <4',
 )
 
