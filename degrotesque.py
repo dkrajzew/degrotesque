2b8eefc5b36cfbd0cd7b2ee0c1783cb6968e8b29
dkrajzew <d.krajzewicz@gmail.com>
2023-01-04 22:28:33 +0100

    updating comments

--- degrotesque.py
+++ degrotesque.py
@@ -1,16 +1,18 @@
 from __future__ import print_function
-"""degrotesque.py
-
-degrotesque - A web type setter, version 2.0.
-
-(c) Daniel Krajzewicz 2020-2023
-daniel@krajzewicz.de
-http://www.krajzewicz.de
-https://github.com/dkrajzew/degrotesque
-http://www.krajzewicz.de/blog/degrotesque.php
-
-Available under the BSD license.
-"""
+# ======================================
+# degrotesque - A web type setter.
+# Version 2.0.
+#
+# Main module
+#
+# (c) Daniel Krajzewicz 2020-2023
+# - daniel@krajzewicz.de
+# - http://www.krajzewicz.de
+# - https://github.com/dkrajzew/degrotesque
+# - http://www.krajzewicz.de/blog/degrotesque.php
+# 
+# Available under the BSD license.
+# ======================================
 
 
 # --- imports -------------------------------------------------------
