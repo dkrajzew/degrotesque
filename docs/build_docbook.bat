759c44f5b6de8d27cdc8bac36adf0fc08714504e
dkrajzew <d.krajzewicz@gmail.com>
2023-03-19 16:43:49 +0100

    removing docbook for now

--- docs/build_docbook.bat
+++
@@ -1,5 +0,0 @@
-%DKBUILD_DOCBOOK%\libxslt-1.1.26.win32\bin\xsltproc.exe --output man-degrotesque.html single_html.xsl man-degrotesque.xml
-%DKBUILD_DOCBOOK%\libxslt-1.1.26.win32\bin\xsltproc.exe --output man-degrotesque.fo single_fo.xsl man-degrotesque.xml
-call %DKBUILD_DOCBOOK%\fop-2.6\fop\fop man-degrotesque.fo -pdf man-degrotesque.pdf
-
-
