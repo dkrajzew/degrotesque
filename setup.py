1211526ab4782d3585396fbb2b6edfd7338cfa18
dkrajzew <d.krajzewicz@gmail.com>
2024-09-09 19:22:22 +0200

    correcting the setup

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ setuptools.setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
